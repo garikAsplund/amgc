f0facf07e41832e33f772aeac4e254ca8d16fc28
Garik Asplund <garik.asplund@gmail.com>
2024-11-05 22:20:26 -0800

    adding dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,14 @@
 	import Footer from '$lib/Footer.svelte';
 	import Nav from '$lib/Nav.svelte';
 	import '../app.css';
+	import { page } from '$app/stores';
 
 	let { children } = $props();
 </script>
 
 <div class="flex h-full min-h-screen w-full flex-col">
 	<Nav />
-	<div class="instet-0 relative w-full flex-grow overflow-hidden dark:bg-[#121212]">
+	<div class="instet-0 relative w-full flex-grow overflow-hidden {$page.url.pathname === '/' ? '' : 'dark:bg-[#121212]'}">
 		{@render children()}
 	</div>
 	<Footer />
