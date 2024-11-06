49aa63cf61d8d6d34903c663dc6970ff6c76ce09
Garik Asplund <garik.asplund@gmail.com>
2024-11-05 22:58:17 -0800

    adding dark mode

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -6,19 +6,32 @@
 </script>
 
 <div
-	class="sticky top-0 flex w-full items-center justify-between space-x-4 px-16 py-4 font-serif font-light text-gray-800 bg-white dark:bg-[#121212] dark:text-gray-200 z-10"
+	class="sticky top-0 flex w-full items-center justify-between px-16 py-4 font-serif font-light text-gray-800 bg-white dark:bg-[#121212] dark:text-gray-200 z-10"
 	id="nav"
 >
-	<div class="">
+	<!-- Left side (Logo) -->
+	<div class="flex-shrink-0">
 		<a href="/" aria-label="Home" class="hover:opacity-75"><LogoSvg /> </a>
 	</div>
-	<div class="flex justify-center space-x-8 text-2xl">
-		<a href="/course" class="hover:opacity-75 p-4 {$page.url.pathname === '/course' ? 'border-b-2 border-slate-600 ' : ''}">Course</a>
-		<a href="/rates" class="hover:opacity-75 p-4 {$page.url.pathname === '/rates' ? 'border-b-2 border-slate-600 ' : ''}">Rates</a>
-		<a href="/events" class="hover:opacity-75 p-4 {$page.url.pathname === '/events' ? 'border-b-2 border-slate-600 ' : ''}">Events</a>
-		<a href="/team" class="hover:opacity-75 p-4 {$page.url.pathname === '/team' ? 'border-b-2 border-slate-600 ' : ''}">Our Team</a>
+
+	<!-- Centered navigation links -->
+	<div class="absolute inset-x-0 flex justify-center space-x-8 text-2xl" id="links">
+		<a href="/course" class="hover:opacity-75 p-4 {$page.url.pathname === '/course' ? 'border-b-2 border-slate-600 ' : ''}">
+			Course
+		</a>
+		<a href="/rates" class="hover:opacity-75 p-4 {$page.url.pathname === '/rates' ? 'border-b-2 border-slate-600 ' : ''}">
+			Rates
+		</a>
+		<a href="/events" class="hover:opacity-75 p-4 {$page.url.pathname === '/events' ? 'border-b-2 border-slate-600 ' : ''}">
+			Events
+		</a>
+		<a href="/team" class="hover:opacity-75 p-4 {$page.url.pathname === '/team' ? 'border-b-2 border-slate-600 ' : ''}">
+			Our Team
+		</a>
 	</div>
-	<div class="flex items-center space-x-4">
+
+	<!-- Right side (Weather and Socials) -->
+	<div class="flex-shrink-0 flex items-center space-x-4">
 		<WeatherWidget />
 		<Socials />
 	</div>
