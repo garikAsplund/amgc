8bdd31119506fd821987d0a5f03e35a9c8327544
Garik Asplund <garik.asplund@gmail.com>
2024-11-12 07:25:20 -0800

    fix dynamic link on weather widget

--- src/lib/WeatherWidget.svelte
+++ src/lib/WeatherWidget.svelte
@@ -27,12 +27,10 @@
 	href="https://www.google.com/search?q=enterprise+or+weather&sourceid=chrome&ie=UTF-8"
 	target="_blank"
 	rel="noopener"
-	class="hover:opacity-75"
+	class="flex items-center gap-2 p-2 hover:opacity-75"
 >
-	<div class="flex items-center gap-2 p-2">
-		{#if $page.data.weather}
-			<span class="text-2xl">{weatherEmojis[$page.data.weather.icon]}</span>
-			<span class=" opacity-80">{$page.data.weather.temp}°</span>
-		{/if}
-	</div>
+	{#if $page.data.weather}
+		<span class="text-2xl">{weatherEmojis[$page.data.weather.icon]}</span>
+		<span class=" opacity-80">{$page.data.weather.temp}°</span>
+	{/if}
 </a>
