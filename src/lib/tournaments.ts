5c534140eb2f751411ea89ea21c6446dbce3f6dc
Garik Asplund <garik.asplund@gmail.com>
2025-04-30 20:42:30 -0700

    updated tournaments

--- src/lib/tournaments.ts
+++ src/lib/tournaments.ts
@@ -1,94 +1,92 @@
 export const tournaments = [
 	{
-	  month: "May",
-	  events: [
-		{
-		  date: "Friday, May 9th",
-		  name: "High School Golf Tournament",
-		},
-	  ],
+		month: 'May',
+		events: [
+			{
+				date: 'Friday, May 9th',
+				name: 'High School Golf Tournament'
+			}
+		]
 	},
 	{
-	  month: "June",
-	  events: [
-		// {
-		//   date: "Saturday, June 8th",
-		//   name: "Ron Layton Memorial Scramble",
-		//   time: "9:00 a.m. shotgun start",
-		//   price: "$75.00 per person",
-		//   details: "Registration Deadline is 6:00 p.m. June 7th",
-		// },
-		{
-		  date: "Sunday, June 14th",
-		  name: "Father's Day Memorial Tournament",
-		},
-		{
-		  date: "Saturday, June 21st",
-		  name: "AMGC Triple Six Tournament",
-		  details:
-			"2 Person team: 6 holes Best Ball, 6 holes Scramble, 6 holes Chapman",
-		},
-	  ],
+		month: 'June',
+		events: [
+			// {
+			//   date: "Saturday, June 8th",
+			//   name: "Ron Layton Memorial Scramble",
+			//   time: "9:00 a.m. shotgun start",
+			//   price: "$75.00 per person",
+			//   details: "Registration Deadline is 6:00 p.m. June 7th",
+			// },
+			{
+				date: 'Sunday, June 14th',
+				name: "Father's Day Memorial Tournament"
+			},
+			{
+				date: 'Saturday, June 21st',
+				name: 'AMGC Triple Six Tournament',
+				details: '2 Person team: 6 holes Best Ball, 6 holes Scramble, 6 holes Chapman'
+			}
+		]
 	},
 	{
-	  month: "July",
-	  events: [
-		{
-		  date: "Monday, July 21st",
-		  name: "Eastside Seniors Golf Association Tournament",
-		},
-		{
-		  date: "Saturday, July 19th",
-		  name: "Elks Tournament",
-		},
-		{
-		  date: "Friday, July 25th",
-		  name: "Helping Hearts Tournament",
-		  time: "9:00 a.m. shotgun start",
-		  price: "$85/Person",
-		  details: "4-person Scramble",
-		},
-	  ],
+		month: 'July',
+		events: [
+			{
+				date: 'Saturday, July 19th',
+				name: 'Elks Tournament'
+			},
+			{
+				date: 'Monday, July 21st',
+				name: 'Eastside Seniors Golf Association Tournament'
+			},
+			{
+				date: 'Friday, July 25th',
+				name: 'Helping Hearts Tournament',
+				time: '9:00 a.m. shotgun start',
+				price: '$85/Person',
+				details: '4-person Scramble'
+			}
+		]
 	},
 	{
-	  month: "August",
-	  events: [
-		{
-		  date: "Tuesday, August 5th",
-		  name: "Gail Beck Ladies Invitational",
-		  time: "9:00 a.m. shotgun start",
-		},
-		{
-		  date: "Saturday, August 9th",
-		  name: "JAIP Tournament",
-		},
-		{
-		  date: "Saturday & Sunday, August 16th & 17th",
-		  name: "AMGC Club Championship",
-		},
-		{
-		  date: "Saturday & Sunday, August 30th & 31st",
-		  name: "Rat Race",
-		},
-	  ],
+		month: 'August',
+		events: [
+			{
+				date: 'Tuesday, August 5th',
+				name: 'Gail Beck Ladies Invitational',
+				time: '9:00 a.m. shotgun start'
+			},
+			{
+				date: 'Saturday, August 9th',
+				name: 'JAIP Tournament'
+			},
+			{
+				date: 'Saturday & Sunday, August 16th & 17th',
+				name: 'AMGC Club Championship'
+			},
+			{
+				date: 'Saturday & Sunday, August 30th & 31st',
+				name: 'Rat Race'
+			}
+		]
 	},
 	{
-	  month: "September",
-	  events: [
-		{
-		  date: "Saturday, September 13th",
-		  name: "Lions Tournament",
-		},
-	  ],
+		month: 'September',
+		events: [
+			{
+				date: 'Saturday, September 13th',
+				name: 'Lions Tournament'
+			}
+		]
 	},
 	{
-	  month: "October",
-	  events: [
-		{
-		  date: "Monday, October 13th",
-		  name: "Firefighters Tournament",
-		},
-	  ],
-	},
-  ];
-  
+		month: 'October',
+		events: [
+			{
+				date: 'Monday, October 13th',
+				name: 'Firefighters Tournament'
+			}
+		]
+	}
+];
