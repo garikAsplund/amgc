0473cae1555e9c9f305bb32ece0c8d08586e3c66
Garik Asplund <garik.asplund@gmail.com>
2025-05-26 23:00:06 -0700

    updated tourneys to 2025

--- src/routes/tournaments/+page.svelte
+++ src/routes/tournaments/+page.svelte
@@ -4,7 +4,7 @@
 
 <section class="w-full h-full p-8">
 	<div class="max-w-2xl mx-auto space-y-8">
-		<h2 class="text-3xl font-serif text-center dark:text-gray-200">2024 Tournaments</h2>
+		<h2 class="text-3xl font-serif text-center dark:text-gray-200">2025 Tournaments</h2>
 
 		<!-- <div class="bg-red-100 dark:bg-red-900/30 border-l-4 border-red-500 p-4">
 			<p class="font-medium dark:text-gray-200">GOLF COURSE IS CLOSED TO OPEN PLAY ON DAYS OF TOURNAMENTS.</p>
