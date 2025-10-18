b6334321ec398b143749edfe27dde89dad0b4c60
Garik Asplund <garik.asplund@gmail.com>
2025-10-18 14:31:15 -0700

    updated banner to closed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -77,7 +77,7 @@
 			</a>
 		</div>
 		<p class="absolute bottom-0 py-16 font-serif text-xl text-gray-300 md:text-2xl">
-			AMGC closes October 15
+			Closed for the season
 		</p>
 	</div>
 </section>
